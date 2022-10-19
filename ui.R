f5380df42d64f91706f88b9d94514776ed314ff7
Silvana Pesenti <silvana.pesenti@utoronto.ca>
2022-10-19 17:35:09 -0400

    Update ui.R
    update Terms of Service

--- ui.R
+++ ui.R
@@ -653,12 +653,12 @@ p("Shiny: ShinySWIM is powered by the Shiny web application framework (RStudio)"
                           style = "position: relative", 
                           column(width = 12,
                                 h3("Terms of service"),
-                                p("By accessing this web site, you are agreeing to be bound by these
-                                  web site Terms and Conditions of Use, all applicable laws and regulations,
-                                  and agree that you are responsible for compliance with any applicable local 
-                                  laws. If you do not agree with any of these terms, you are prohibited from 
-                                  using or accessing this site. The materials contained in this web site are 
-                                  protected by applicable copyright and trade mark law.")
+                                p("The materials on this website are provided “as is”. The authors of ShinySWIM make no warranties,
+                                  expressed or implied, and hereby disclaim and negate all other warranties, including without limitation, 
+                                  implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement 
+                                  of intellectual property or other violation of rights. Further, the authors of ShinySWIM do not warrant or 
+                                  make any representations concerning the accuracy, likely results, or reliability of the use of the materials 
+                                  on the ShinySWIM website or otherwise relating to such materials or on any sites linked to this site.")
                           )
                         )
                       )
